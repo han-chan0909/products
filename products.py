b5e1074c6de971e0db9d78c267941a6cc1055f23
Han Chan <kong_du@hotmail.com>
2023-09-12 15:18:31 +0800

    first vison

---
+++ products.py
@@ -0,0 +1,14 @@
+products = []
+while True:
+	name = input('請輸入產品名稱')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格')
+	p = [] #清單中的小清單,也可以寫成 p = [name, price]
+	p.append(name)
+	p.append(price)
+	products.append(p) #這個(p)可以直接改成[name, price]
+print (products)
+
+print(products[0][0])#大清單的第0格,小清單的第0格子
+
